d4f1f1b0ddacf4f79b320eaa2159cfec60682130
didemertens <d.g.j.mertens@gmail.com>
2020-04-20 14:10:22 +0100

    fix getGames action, only get the data of response

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,5 +2,5 @@ import rawg from '../apis/rawg'
 
 export const getGames = () => async dispatch => {
     const response = await rawg.get('')
-    dispatch({ type: "GET_GAMES", payload: response })
+    dispatch({ type: "GET_GAMES", payload: response.data })
 }
