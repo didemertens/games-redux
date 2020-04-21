08f0f460ba293bf138f5a7e71ed2f928bcadb427
didemertens <d.g.j.mertens@gmail.com>
2020-04-21 11:45:56 +0100

    add sign in and sign out buttons and functionality

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -13,17 +13,40 @@ class GoogleAuth extends React.Component {
       }).then(() => {
         this.auth = window.gapi.auth2.getAuthInstance()
         this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+        this.auth.isSignedIn.listen(this.onAuthChange)
       })
     })
   }
 
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+  }
+
+  onSignIn = () => {
+    this.auth.signIn()
+  }
+
+  onSignOut = () => {
+    this.auth.signOut()
+  }
+
   renderAuthButton = () => {
     if (this.state.isSignedIn === null) {
-      return <div>Don't know</div>
+      return null
     } else if (this.state.isSignedIn) {
-      return <div>Signed in</div>
+      return (
+      <button onClick={this.onSignOut} className="ui green google button">
+        <i className="google icon" />
+        Sign Out
+      </button>
+      )
     } else {
-      return <div>Not signed in</div>
+      return (
+      <button onClick={this.onSignIn} className="ui green google button">
+        <i className="google icon" />
+        Sign In
+      </button>
+      )
     }
   }
 
