84b0e125e13057dbcaf783d86b335332febdf784
didemertens <d.g.j.mertens@gmail.com>
2020-04-23 12:27:10 +0100

    refactor modal component to make it more general

--- src/components/common/Modal.js
+++ src/components/common/Modal.js
@@ -2,18 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import history from '../../history'
 
-const Modal = props => {
+const Modal = ({ title, content, actions }) => {
   return ReactDOM.createPortal (
     <div onClick={() => history.push('/')} className="ui dimmer modals visible active">
       <div onClick={(e) => e.stopPropagation()} className="ui standard modal visible active">
-        <div className="header">Delete game</div>
-        <div className="content">
-          Are you sure that you want to delete this game?
-        </div>
-        <div className="actions">
-          <button className="ui primary button">Delete</button>
-          <button className="ui button">Cancel</button>
-        </div>
+        <div className="header">{title}</div>
+        <div className="content">{content}</div>
+        <div className="actions">{actions}</div>
       </div>
     </div>,
     document.querySelector('#modal')
--- src/components/games/GameDelete.js
+++ src/components/games/GameDelete.js
@@ -3,9 +3,20 @@ import React from 'react'
 import Modal from '../common/Modal'
 
 const GameDelete = () => {
+  const actions = (
+    <>
+      <button className="ui button negative">Delete</button>
+      <button className="ui button">Cancel</button>
+    </>
+  )
+
   return (
     <div>
-      <Modal />
+      <Modal
+        title="Delete Game"
+        content="Are you sure you want to delete this game?"
+        actions={actions}
+      />
     </div>
   )
 }
